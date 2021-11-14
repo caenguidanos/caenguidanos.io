673f4c0ddb4f58087ed30b222f076c5334c451ce
Cristian Enguídanos Nebot <53697395+cenguidanos@users.noreply.github.com>
2021-11-14 18:49:40 +0100

    feat(prettier): add config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  content: ["./src/**/*.{ts,tsx,js,jsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+   content: ["./src/**/*.{ts,tsx,js,jsx}"],
+   theme: {
+      extend: {}
+   },
+   plugins: []
+};
