71f27824ed0aaaeaee0dac7077e61cbcaecb82e0
Cristian Enguídanos Nebot <53697395+cenguidanos@users.noreply.github.com>
2021-11-16 16:14:05 +0100

    feat(jest): remove empty jest.setup

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,6 @@ const nextJest = require("next/jest");
 const createJestConfig = nextJest({ dir: process.cwd() });
 
 const customJestConfig = {
-   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
    moduleNameMapper: {
       "^\\$styles/(.*)$": ["<rootDir>/src/styles/$1"],
       "^\\$shared/layout$": ["<rootDir>/src/lib/shared/layout/index.ts"],
