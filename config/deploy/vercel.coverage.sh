b8956404eb1db7e2679583899d8bc469f8a9b360
Cristian Enguídanos Nebot <53697395+cenguidanos@users.noreply.github.com>
2021-11-17 23:53:41 +0100

    fix(vercel): invalid build command

--- config/deploy/vercel.coverage.sh
+++ config/deploy/vercel.coverage.sh
@@ -1,3 +1,3 @@
 export VERCEL_PROJECT_ID=$VERCEL_PROJECT_ID_JEST
 
-vercel --local-config ./config/deploy/vercel.coverage.json --production --scope $VERCEL_SCOPE --token $VERCEL_TOKEN
+vercel --local-config ./config/deploy/vercel.coverage.json --scope $VERCEL_SCOPE --token $VERCEL_TOKEN
