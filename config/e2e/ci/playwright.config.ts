32197fe50f34217c75944e8302f53c96a0d70b59
Cristian Enguídanos Nebot <53697395+cenguidanos@users.noreply.github.com>
2021-11-17 23:43:30 +0100

    feat(ci-cd): create multiple workloads

--- config/e2e/ci/playwright.config.ts
+++ config/e2e/ci/playwright.config.ts
@@ -1,20 +1,43 @@
 import path from "path";
 
-import { PlaywrightTestConfig } from "@playwright/test";
+import { PlaywrightTestConfig, devices } from "@playwright/test";
 
 const config: PlaywrightTestConfig = {
    globalSetup: require.resolve("./playwright.global.ts"),
    globalTeardown: require.resolve("./playwright.teardown.ts"),
+   preserveOutput: "failures-only",
+   projects: [
+      {
+         name: "Desktop Edge",
+         use: {
+            ...devices["Desktop Edge"]
+         }
+      },
+      {
+         name: "Desktop Chrome",
+         use: {
+            ...devices["Desktop Chrome"]
+         }
+      },
+      {
+         name: "Desktop Firefox",
+         use: {
+            ...devices["Desktop Firefox"]
+         }
+      },
+      {
+         name: "Mobile Chrome",
+         use: {
+            ...devices["Pixel 5"]
+         }
+      }
+   ],
    reporter: [["html", { outputFolder: "coverage-e2e" }], ["list"]],
-   retries: 3,
+   retries: 2,
    testDir: path.resolve(process.cwd(), "src"),
    testMatch: "**/*.e2e.ts",
    timeout: 30000,
-   use: {
-      baseURL: "https://caenguidanos-io.vercel.app",
-      browserName: "chromium",
-      headless: true
-   }
+   use: { baseURL: process.env.PLAYWRIGHT_BASE_URL }
 };
 
 export default config;
