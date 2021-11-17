b7f0b0f3ec8ad560aa5e8ce5f212279e40c1222b
Cristian Enguídanos Nebot <53697395+cenguidanos@users.noreply.github.com>
2021-11-17 22:39:50 +0100

    fix(playwright): change baseURL when ci config to target github env

--- config/e2e/ci/playwright.config.ts
+++ config/e2e/ci/playwright.config.ts
@@ -53,7 +53,7 @@ const config: PlaywrightTestConfig = {
    testDir: path.resolve(process.cwd(), "src"),
    testMatch: "**/*.e2e.ts",
    timeout: 30000,
-   use: { baseURL: process.env.PLAYWRIGHT_TEST_BASE_URL + ":3000" },
+   use: { baseURL: process.env.PLAYWRIGHT_TEST_BASE_URL },
    webServer: {
       command: "./node_modules/next/dist/bin/next start",
       port: 3000,
