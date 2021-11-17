c71d3a86730357b1e147a940de141cecf44f4771
Cristian Enguídanos Nebot <53697395+cenguidanos@users.noreply.github.com>
2021-11-17 23:09:36 +0100

    feat(ci-cd): change playwright config

--- config/e2e/ci/playwright.config.ts
+++ config/e2e/ci/playwright.config.ts
@@ -1,59 +1,21 @@
 import path from "path";
 
-import { PlaywrightTestConfig, devices } from "@playwright/test";
+import { PlaywrightTestConfig } from "@playwright/test";
 
 const config: PlaywrightTestConfig = {
    globalSetup: require.resolve("./playwright.global.ts"),
    globalTeardown: require.resolve("./playwright.teardown.ts"),
-   preserveOutput: "failures-only",
-   projects: [
-      {
-         name: "Desktop Edge",
-         use: {
-            locale: "es-ES",
-            trace: "retain-on-failure",
-            video: "retain-on-failure",
-            screenshot: "only-on-failure",
-            ...devices["Desktop Edge"]
-         }
-      },
-      {
-         name: "Desktop Chrome",
-         use: {
-            locale: "es-ES",
-            trace: "retain-on-failure",
-            video: "retain-on-failure",
-            screenshot: "only-on-failure",
-            ...devices["Desktop Chrome"]
-         }
-      },
-      {
-         name: "Desktop Firefox",
-         use: {
-            locale: "es-ES",
-            trace: "retain-on-failure",
-            video: "retain-on-failure",
-            screenshot: "only-on-failure",
-            ...devices["Desktop Firefox"]
-         }
-      },
-      {
-         name: "Mobile Chrome",
-         use: {
-            locale: "es-ES",
-            trace: "retain-on-failure",
-            video: "retain-on-failure",
-            screenshot: "only-on-failure",
-            ...devices["Pixel 5"]
-         }
-      }
-   ],
    reporter: [["html", { outputFolder: "coverage-e2e" }], ["list"]],
-   retries: 2,
+   retries: 3,
    testDir: path.resolve(process.cwd(), "src"),
    testMatch: "**/*.e2e.ts",
    timeout: 30000,
-   use: { baseURL: "https://caenguidanos-io.vercel.app" }
+   use: {
+      baseURL: "https://caenguidanos-io.vercel.app",
+      browserName: "chromium",
+      headless: true,
+      locale: "es-ES"
+   }
 };
 
 export default config;
