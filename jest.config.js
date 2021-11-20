493c2e3588dbd3bde890f051f17ea5f965221927
Cristian Enguídanos Nebot <53697395+cenguidanos@users.noreply.github.com>
2021-11-20 16:23:38 +0100

    feat(workspace): prepare development

--- jest.config.js
+++ jest.config.js
@@ -4,16 +4,17 @@ const createJestConfig = nextJest({ dir: process.cwd() });
 
 /** @type {import('@jest/types').Config.InitialOptions} */
 const customJestConfig = {
-   verbose: true,
    bail: 1,
    errorOnDeprecated: true,
+   extensionsToTreatAsEsm: [".ts", ".tsx"],
+   extraGlobals: ["Math"],
    coverageDirectory: "./dist/tests/unit/reports",
    coverageReporters: ["html-spa"],
    coverageThreshold: {
       global: {
          branches: 75,
          functions: 100,
-         lines: 100,
+         lines: 75,
          statements: 0
       }
    },
@@ -22,7 +23,7 @@ const customJestConfig = {
       "^\\$shared/layout$": ["<rootDir>/src/lib/shared/layout/index.ts"],
       "^\\$shared/markdown$": ["<rootDir>/src/lib/shared/markdown/index.ts"]
    },
-   extraGlobals: ["Math"]
+   verbose: true
 };
 
 module.exports = createJestConfig(customJestConfig);
--- src/lib/shared/markdown/util/transform-to-html/transform-to-html.ts
+++ src/lib/shared/markdown/util/transform-to-html/transform-to-html.ts
@@ -1,28 +1,42 @@
-import unified from "unified";
+import { unified } from "unified";
 import remarkParse from "remark-parse";
 import remarkGfm from "remark-gfm";
 import remarkRehype from "remark-rehype";
 import rehypeStringify from "rehype-stringify";
 import rehypeRaw from "rehype-raw";
-import path from "path";
+import { getHighlighter, Highlighter } from "shiki";
 
-import { getHighlighter } from "shiki";
-
-import remarkShiki from "../plugins/shiki-remark/plugin.js";
+import remarkShiki from "../plugins/shiki-remark/plugin";
 import remarkShikiTheme from "../plugins/shiki-remark/theme.json";
 
 interface TransformToHtmlOptions {
    input: string;
 }
 
-export async function transformToHtml(options: TransformToHtmlOptions): Promise<string> {
-   const highlighter = await getHighlighter({
-      theme: remarkShikiTheme as unknown as string
+function cacheHighlighter(): Promise<Highlighter> {
+   let highlighter: Highlighter;
+
+   return new Promise<Highlighter>((resolve) => {
+      if (!highlighter) {
+         getHighlighter({
+            theme: remarkShikiTheme as unknown as string
+         }).then((nextHighlighter) => {
+            highlighter = nextHighlighter;
+
+            resolve(highlighter);
+         });
+      } else {
+         resolve(highlighter);
+      }
    });
+}
+
+export async function transformToHtml(options: TransformToHtmlOptions): Promise<string> {
+   const highlighter: Highlighter = await cacheHighlighter();
 
    const file = await unified()
       .use(remarkParse)
-      .use(remarkShiki({ highlighter }))
+      .use(remarkShiki, { highlighter })
       .use(remarkGfm)
       .use(remarkRehype, { allowDangerousHtml: true })
       .use(rehypeRaw)
