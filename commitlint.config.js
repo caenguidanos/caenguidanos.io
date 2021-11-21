dd61cb431d8a8847875b26a1a68f1683d1e58dc8
Cristian Enguídanos Nebot <53697395+cenguidanos@users.noreply.github.com>
2021-11-21 02:24:04 +0100

    build(commitlint): add type to config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,21 @@
-module.exports = {
-   extends: ["@commitlint/config-conventional"]
+/**
+ * @type {import('@commitlint/types').UserConfig}
+ */
+const config = {
+   /*
+    * Resolve and load @commitlint/config-conventional from node_modules.
+    * Referenced packages must be installed
+    */
+   extends: ["@commitlint/config-conventional"],
+   /*
+    * Resolve and load @commitlint/format from node_modules.
+    * Referenced package must be installed
+    */
+   formatter: "@commitlint/format",
+   /*
+    * Custom URL to show upon failure
+    */
+   helpUrl: "https://github.com/conventional-changelog/commitlint/#what-is-commitlint"
 };
+
+module.exports = config;
