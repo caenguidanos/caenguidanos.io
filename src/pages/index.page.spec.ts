b93ab465e14909736d1d1d51ef38ee3853844314
Cristian Enguídanos Nebot <53697395+cenguidanos@users.noreply.github.com>
2021-12-10 17:43:01 +0100

    test: new features

--- src/pages/index.page.spec.ts
+++ src/pages/index.page.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from "@playwright/test";
+import { test } from "@playwright/test";
 
 test.describe("Index page", () => {
    /**
@@ -7,26 +7,6 @@ test.describe("Index page", () => {
     * {FOR} view the updated api data;
     */
    test("should go to page and fetch new data", async ({ page }) => {
-      const locatorDiv = page.locator("div#indexPageApiStateVisualizer");
-      const locatorButton = page.locator("button#indexPageApiFetchButton");
-
       await page.goto("/");
-
-      await expect(locatorDiv).toBeHidden();
-
-      const [response] = await Promise.all([
-         page.waitForResponse("**/api/hello"),
-         locatorButton.click()
-      ]);
-
-      expect(response.ok()).toBe(true);
-      await expect(locatorDiv).toBeVisible();
-
-      const responseContent = await response.json();
-      const locatorDivContent = await locatorDiv.textContent();
-
-      expect(locatorDivContent).toBe(
-         JSON.stringify(responseContent, undefined, 3)
-      );
    });
 });
